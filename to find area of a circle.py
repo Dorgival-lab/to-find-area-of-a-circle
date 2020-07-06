0cd56ca6cc0b71b7ef992c61f7cfc568515e6bb2
Dorgival Fernando <59142443+Dorgival-lab@users.noreply.github.com>
2020-07-06 07:14:56 -0300

    Create to find area of a circle.py
    Programa Python para encontrar a área de um círculo

---
+++ to find area of a circle.py
@@ -0,0 +1,10 @@
+# Programa em Python para encontrar a área de um círculo
+  
+def findArea(r): 
+    PI = 3.142
+    return PI * (r*r); 
+  
+# Driver method 
+print("Area is %.6f" % findArea(5)); 
+  
+# Este código é contribuído por Dorgival Fernando 
